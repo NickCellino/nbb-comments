13a7761bc969e5beff6c09ff320a25f2f257d1ec
Nicholas Cellino <nacellin@buffalo.edu>
2022-08-21 22:25:47 -0400

    convert all the html generation to use the hiccup-like function

--- comments/html.cljs
+++ comments/html.cljs
@@ -2,35 +2,34 @@
   (:require [clojure.string :as string]
             [comments.hiccup-clone :refer [html]]))
 
-(html [:p {:class "name"} "<div>hello world</div>"])
-
-(html [:p {:class "name"} [:strong "nick"] "said..."])
-
-; TODO escape html
-(defn get-author-html
-  [comment-body]
-  (let [author-name (or (:author comment-body) "Anonymous")]
-    (html [:p {:class "name"} [:strong author-name] "said..."])))
-
 (defn serialize-comment
   [comment-body]
-  (let [header "<div class=\"comment\">"
-        author-section (str "\t" (get-author-html comment-body))
-        message-section (str "\t<p class=\"message\">" (:message comment-body) "</p>")
-        footer "</div>"]
-    (string/join "\n" [header author-section message-section footer])))
+  (let [author-section [:p {:class "name"} [:strong (or (:author comment-body) "Anonymous")] "said..."]
+        message-section [:p {:class "message"} (:message comment-body)]]
+    [:div {:class "comment"} author-section message-section]))
 
 (defn serialize-comment-list
   [comments]
   (let [serialized-comments (map serialize-comment comments)]
-    (string/join "\n" serialized-comments)))
+    [:div {:id "comment-list"} serialized-comments]))
  
 (def comments-form
-  (str
-    "<form id=\"comment-form\" hx-post=\"/comments\" hx-swap=\"afterbegin\" hx-target=\"#comments\" hx-swap-oob=\"true\">"
-    "\t<label for=\"name\">Name (optional)</label>"
-    "\t<input type=\"text\" name=\"name\">"
-    "\t<label for=\"message\">Message</label>"
-    "\t<textarea name=\"message\" required rows=5></textarea>"
-    "\t<button type=\"submit\">submit</button>"
-    "</form>"))
+  [:form
+   {:id "comment-form" :hx-post "/comments" :hx-swap "afterbegin" :hx-target "#comment-list" :hx-swap-oob "true"}
+   [:label {:for "author"} "Name (optional)"]
+   [:input {:type "text" :name "author"}]
+   [:label {:for "message"} "Comment"]
+   [:textarea {:name "message" :required true :rows 5}]])
+
+(comment
+  (html [:p {:class "name"} "<div>hello world</div>"])
+  (html [:p {:class "name"} [:strong "nick"] "said..."])
+
+  (html (serialize-comment {:author "Nicholas" :message "hello world!"}))
+  (html (serialize-comment {:message "hello world!"}))
+  (html (serialize-comment {:message "<script>alert('you got pwned')</script>"}))
+
+  (html (serialize-comment-list [{:author "Nicholas" :message "hello world!"}
+                                 {:message "hello world!"}
+                                 {:message "yo"}
+                                 {:message "sup"}])))
